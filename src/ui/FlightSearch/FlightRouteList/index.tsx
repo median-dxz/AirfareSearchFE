57d5cd02642ab5e3b45aac42d552b09bdbc1deb8
median-dxz <med_iridescence@163.com>
2023-07-16 18:09:20 +0800

    fix: 修复添加新搜索按钮的位置错误

--- src/ui/FlightSearch/FlightRouteList/index.tsx
+++ src/ui/FlightSearch/FlightRouteList/index.tsx
@@ -4,7 +4,7 @@ import Box from "@/components/Box";
 
 export default function FlightRouteList() {
   return (
-    <Box id="flight-route-group" className="p-2">
+    <Box id="flight-route-group" className="p-2" stack>
       <ul className="space-y-2 mb-2">
         <FlightRouteItem />
       </ul>
