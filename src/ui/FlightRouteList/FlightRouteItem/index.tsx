684b4c26ebd4f9ffe0d758af9835e9499ad726bc
median-dxz <med_iridescence@163.com>
2023-07-25 16:22:20 +0800

    style: 微调搜索项的样式

--- src/ui/FlightRouteList/FlightRouteItem/index.tsx
+++ src/ui/FlightRouteList/FlightRouteItem/index.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/Button";
 import RouteItem from "@/components/Search/RouteItem";
-import PaperAirplaneIcon from "@heroicons/react/24/outline/PaperAirplaneIcon";
+import ArrowPathIcon from "@heroicons/react/24/outline/ArrowPathIcon";
 import TrashIcon from "@heroicons/react/24/outline/TrashIcon";
 import { CitySelect } from "./CitySelect";
 import { DateSelect } from "./DateSelect";
@@ -8,23 +8,21 @@ import { DateSelect } from "./DateSelect";
 export function FlightRouteItem() {
   return (
     <RouteItem>
-      <span className="m-1 inline-block">
+      <span className="inline-block">
         <CitySelect />
       </span>
 
-      <span className="inline-block w-8 text-primary">
-        <PaperAirplaneIcon />
-      </span>
+      <ArrowPathIcon className="inline-block w-8 text-primary" />
 
-      <span className="m-1 inline-block">
+      <span className="inline-block">
         <CitySelect />
       </span>
 
-      <span className="m-1 inline-block">
+      <span className="inline-block">
         <DateSelect />
       </span>
 
-      <Button color="secondary" iconOnly className="m-2">
+      <Button color="secondary" iconOnly>
         <TrashIcon height={16} />
       </Button>
     </RouteItem>
--- src/ui/FlightRouteList/NewRouteButton.tsx
+++ src/ui/FlightRouteList/NewRouteButton.tsx
@@ -3,7 +3,7 @@ import PlusIcon from "@heroicons/react/24/solid/PlusIcon";
 
 export function NewRouteButton() {
   return (
-    <Button color="secondary" className="w-1/3 min-w-fit" iconOnly>
+    <Button color="secondary" className="min-w-fit" iconOnly>
       <PlusIcon className="w-6" />
     </Button>
   );
